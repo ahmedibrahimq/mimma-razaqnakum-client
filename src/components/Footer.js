d8b28022890d7030f2aac9b6d9828c48d8ad0122
ahmedibrahimq <ahmedimiq@gmail.com>
2020-06-27 17:07:53 +0200

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full bg-white text-right p-4">
+      Built by{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.github.com/ahmedibrahimq"
+        className="underline"
+      >
+        Ahmed Ibrahim Mah
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
