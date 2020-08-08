c0b567e1e6972b9d8d9d72c03247042e2dbb7086
ahmedibrahimq <ahmedimiq@gmail.com>
2020-08-08 15:11:12 +0200

    Update API calls. Add methods to add, update, and remove campaigns

--- src/utils/api.js
+++ src/utils/api.js
@@ -21,3 +21,37 @@ export const getGroupCampaigns = (group, page, limit = 10) => {
 export const getCampaign = (title, group) => {
   return axios.get(`http://localhost:3000/api/campain/${title}?gn=${group}`);
 };
+
+export const addCampaign = (group, campaign) => {
+  const data = {
+    title: campaign.title,
+    group_name: group,
+    details: campaign.details,
+    expires_at: campaign.expires_at,
+    money_target: campaign.money_target,
+    pics: campaign.pics || [],
+    tags: campaign.tags || ["other"],
+  };
+  return axios.post(`http://localhost:3000/api/campain`, data);
+};
+
+export const updateCampaign = (title, group, campaign) => {
+  const data = {
+    title: title,
+    group_name: group,
+    details: campaign.details,
+    expires_at: campaign.expires_at,
+    status: campaign.status,
+    pics: campaign.pics || [],
+    // tags: campaign.tags || ["other"],
+  };
+  return axios.put(`http://localhost:3000/api/campain/${title}`, data);
+};
+
+export const deleteCampaign = (title, group) => {
+  const data = {
+    title: title,
+    groupName: group,
+  };
+  return axios.delete(`http://localhost:3000/api/campain/${title}`, { data });
+};
